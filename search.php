64912cd2a3426da700a86821b1da49d1ac91756e
RoGGeR <warlack@outlook.com>
2017-06-06 17:43:37 +0300

    login object
    i
    X

---
+++ search.php
@@ -0,0 +1,10 @@
+<?
+    ini_set('display_errors',1);
+	error_reporting(E_ALL);
+	header("Content-Type: text/html; charset=utf-8");
+	include_once('db_connect.php');
+
+	$answer = json_decode(file_get_contents('php://input'), true);
+	echo json_encode($answer);
+
+?>
