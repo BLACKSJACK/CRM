66d3b81052d83fa7f5e88506ccf9a45f0c69d703
RoGGeR <warlack@outlook.com>
2017-06-21 17:21:01 +0300

    full completed search for companies

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -2,21 +2,73 @@
  * Created by RoGGeR on 14.06.17.
  */
 app.controller('searchCtrl', function($scope,$rootScope,$http,$q){
+    $rootScope.cacheTemplate={};
+    $scope.isEmptyObject = function(obj) {//функция проверки объекта на пустоту
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    $scope.searchFilter=function(values){
 
+        for(var i=0; i<values.length; i++){
+            var obj=values[i];
+            $rootScope.cacheTemplate[obj.model]=obj.val;
+        }
+
+    };
+    $scope.template={};//объект шаблон, необходимый для запроса к бд и дальнейшему решению искать ли в кэше или заново обращаться к бд
+    $scope.checkTemplate=function(values){
+        var obj;
+        for(var i=0;i<values.length;i++){
+            if(values[i].model===$scope.template.model){
+
+                obj=values[i];
+                i=values.length;
+            }
+        }
+        if(obj) return obj.val.search($scope.template.txt)==0;
+        else return false;
+    };
     $scope.search = function( values , type) {
-        if($scope.abort) {
+        var data={};
+
+        data.type=type;
+        if($scope.abort){
             $scope.abort.resolve();
         }
         $scope.abort = $q.defer();
-        var data={};
-        data.values=values;
-        data.type=type;
+        var flag = values.find(function(element){// функция проверяет введено ли хоть в одно поле поиска значение, если нет - обнуляется массив
+            return element.val != '' && element.val!=undefined && element.val.length>1
+        });
+        data.value=flag;
+        console.log(data);
+        $scope.template.txt=flag.val;
+        $scope.template.model=flag.model;
         $http.post("search.php", data,{timeout:$scope.abort.promise}).then(function success (response) {
+
                 console.log(response.data);
+
                 $rootScope.search_result=response.data;
             },function error (response){
                 console.log("Request cancelled");
             }
         );
     };
+    $scope.clean=function(){//очищаем все результаты поиска
+        $rootScope.search_result=[];//<==== обнуляется массив
+        $scope.template={};
+    };
+    $scope.isEmptyQuery=function(values){
+
+        var data={};
+        data.values=values;
+        var flag = data.values.find(function(element){// функция проверяет введено ли хоть в одно поле поиска значение, если нет - обнуляется массив
+            return element.val != '' && element.val!=undefined && element.val.length>1
+        });
+        return flag;
+    }
+
 });
--- search.php
+++ search.php
@@ -6,6 +6,7 @@
 	include_once('db_connect.php');
 
 	$data = json_decode(file_get_contents('php://input'), true);
+	$value=$data['value'];
 	if($data['type']=="find_company"){
 
 
@@ -14,16 +15,11 @@
 	    $query.="LEFT JOIN Companies as com on con.company_id = com.id ";
 	    $query.="LEFT JOIN Contacts as ctx on con.contact_id = ctx.id ";
 	    $query.="LEFT JOIN addresses as addr on addr.id = com.Legal_address ";
-	    $query.="WHERE ";
-	    $index=0;
-	    foreach ($data['values'] as $value){
-	        if(isset($value["val"]) && $value["val"]!="" && $value["db"]=="companies"){
-	            if($index>0) $query.=" AND ";
-	            $query.="LOWER(com.".$value['model'].") RLIKE LOWER('".$value['val']."')";
-	            $index++;
-	            $companies=true;
-	        }
-	    }
+	    //$query.="WHERE LOWER(".$data['value']['as'].".".$data['value']['model'].") RLIKE LOWER('".$data['value']['val']."')";
+
+	    if($value['db']=='companies') $query.="WHERE LOWER(com.".$value['model'].") RLIKE LOWER('".$value['val']."')";
+	    else if($value['db']=='contacts') $query.="WHERE LOWER(ctx.FirstName) RLIKE LOWER('".$value['val']."') OR LOWER(ctx.LastName) RLIKE LOWER('".$value['val']."')";
+        else if($value['db']=='addresses') $query.="WHERE LOWER(addr.City) RLIKE LOWER('".$value['val']."') OR LOWER(addr.Street) RLIKE LOWER('".$value['val']."')";
         //echo $query;
         $result = mysql_query($query) or die(mysql_error());
         $resultJson = array();
@@ -33,6 +29,15 @@
            $resultJson[]=$row;
 
 
+        }
+        $array=[];
+        foreach($resultJson as $key=>$value){
+            $resultJson[$key]['contact']=$resultJson[$key]['FirstName']." ".$resultJson[$key]['LastName'];
+            unset($resultJson[$key]['LastName']);
+            unset($resultJson[$key]['FirstName']);
+            $resultJson[$key]['Legal_address']=$resultJson[$key]['City']." ".$resultJson[$key]['Street'];
+            unset($resultJson[$key]['City']);
+            unset($resultJson[$key]['Street']);
         }
 
         echo json_encode($resultJson);
