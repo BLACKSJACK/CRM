2fb0fec4aa34e67b5245c5a82e6238ae6712fc3f
RoGGeR <warlack@outlook.com>
2017-07-19 16:20:11 +0300

    almost all json for calculator

--- js/controllers/transportation_calc.js
+++ js/controllers/transportation_calc.js
@@ -12,6 +12,27 @@ app.controller("tCalcCtrl", function ($http) {
     };
     var koef_pow;
     console.log(Points);
+    this.Process=function(cost, park, wrapping, risk, limit, franchise){//конструктор процессов
+        this.cost=cost;
+        this.park=park;
+        this.wrapping=wrapping;
+        this.risk=risk;
+        this.limit=limit;
+        this.franchise=franchise;
+    };
+    this.Process.prototype.calculate=function(){//расчет
+        this.amount=this.cost*this.park;
+        var spline = Spline(this.cost, Points.cost, 1);
+        var spline1 = Spline(parks.amount, Points.amount, 0);    // park заменили на dop_park
+        var price = spline*(1+spline1/100);
+        price *= Franchise(this.cost, this.franchise);
+        if(this.cost>=this.limit){
+            price*=Limit(this.limit*parks.amount*(1+((this.cost-this.limit)/this.limit)), this.limit);// park заменили на dop_park
+        }
+        else price*=Limit(this.cost*parks.amount*(1+((this.limit-this.cost)/this.limit)), this.limit);// park заменили на dop_park
+        this.stavka=price;
+        this.premia=this.amount*price/100;
+    };
     $http.post("loadPoints.php").then(function success(response){
         console.log(response.data);
         var ar=response.data;
@@ -26,23 +47,19 @@ app.controller("tCalcCtrl", function ($http) {
         SplineKoeff(1, Points.cost); //интерполируем как жОские пацаны
         SplineKoeff(2, Points.risk); //продолжаем интерполировать как жОские пацаны
         SplineKoeff(3, Points.payment);
-
+        var proc1=new scope.Process(5000000,120,0,0,5000000,0);
+        proc1.calculate();
+        console.log(proc1);
 
     }, function error(response) {
         console.log(response.data);
         console.log(response.data);
     });
 
+    var parks={};
+    parks.amount=120;
 
-    this.Process=function(cost, amount, package, risk, limit, franchise){//конструктор процессов
-        this.cost=cost;
-        this.amount=amount;
-        this.package=package;
-        this.risk=risk;
-        this.limit=limit;
-        this.franchise=franchise;
-    };
-    this.Process.prototype.calculate=function(){//расчет
 
-    }
+
+
 });
