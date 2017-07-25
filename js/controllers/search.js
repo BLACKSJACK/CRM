78c8aa78e1e395d87873dcde9c8da9eeb393bdeb
RoGGeR <warlack@outlook.com>
2017-07-25 13:40:13 +0300

    refixed search

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -16,15 +16,22 @@ app.controller('searchCtrl', function($rootScope,$http,$q,$location,myFactory){
     };
 
     this.searchFilter=function(values){
-
+        console.log(values);
         for(var i=0; i<values.length; i++){
             var obj=values[i];
-            $rootScope.cacheTemplate[obj.model]=obj.val;
+            if(obj.val=="") $rootScope.cacheTemplate[obj.model]=undefined;
+            else if(obj.model=="contact"){
+                $rootScope.cacheTemplate.contact={};
+                if(isNaN(obj.val)) $rootScope.cacheTemplate.contact["name"]=obj.val;
+                else $rootScope.cacheTemplate.contact["phone"]=obj.val;
+            }
+            else $rootScope.cacheTemplate[obj.model]=obj.val;
+            console.log($rootScope.cacheTemplate);
         }
 
     };
     this.template={};//объект шаблон, необходимый для запроса к бд и дальнейшему решению искать ли в кэше или заново обращаться к бд
-    this.checkTemplate=function(values){
+    this.checkTemplate=function(values){//проверка шаблона
         var obj;
         for(var i=0;i<values.length;i++){
             if(values[i].model===scope.template.model){
@@ -32,6 +39,7 @@ app.controller('searchCtrl', function($rootScope,$http,$q,$location,myFactory){
                 obj=values[i];
                 i=values.length;
             }
+            console.log(scope.template);
         }
         if(obj) return obj.val.search(scope.template.txt)==0;
         else return false;
@@ -45,23 +53,23 @@ app.controller('searchCtrl', function($rootScope,$http,$q,$location,myFactory){
             scope.abort.resolve();
         }
         scope.abort = $q.defer();
-        var flag = values.find(function(element){// функция проверяет введено ли хоть в одно поле поиска значение, если нет - обнуляется массив
-            return element.val != '' && element.val!=undefined && element.val.length>1
-        });
+        var flag=this.isEmptyQuery(values);
         data.value=flag;
 
         scope.template.txt=flag.val;
         scope.template.model=flag.model;
         console.log(data);
+        console.log(scope.template);
         $http.post("search.php", data,{timeout:scope.abort.promise}).then(function success (response) {
 
 
 
                 scope.myFactory.matrixType=type;
 
-
+                $rootScope.cacheTemplate={};
 
                 $rootScope.search_result=response.data;
+                console.log($rootScope.search_result);
             },function error (response){
                 console.log(response);
             }
